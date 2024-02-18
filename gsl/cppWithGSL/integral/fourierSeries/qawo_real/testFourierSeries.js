2cefa602e843feba56490bd15476de0df41aeb22
Victor Chen <cyavictor88@gmail.com>
2024-02-18 11:14:46 +0700

    added qawf/qawo for FT/FS , and reorganized folder

---
+++ gsl/cppWithGSL/integral/fourierSeries/qawo_real/testFourierSeries.js
@@ -0,0 +1,64 @@
+var factory = require('./example.js');
+
+
+async function getFS(functionToIntegrate, maxK, lowerBound, upperBound) {
+  return factory().then((instance) => {
+    const ak = [];
+    const bk = [];
+    const period = upperBound - lowerBound;
+
+    let start = 0;
+    let end = maxK;
+
+    let ks = [];
+    for (let i = start; i <= end; i += 1) {
+      ks.push(i);
+    }
+
+    // function functionToIntegrate(x) {
+    //   return x**3-12*x+16;
+    //   // return x**2+Math.sin(x);
+    // }
+    instance['jsFunc'] = functionToIntegrate;
+
+    for (let i = 0; i < ks.length; i++) {
+      const k = ks[i];
+      const resPtr = instance._approxFourierSeries(lowerBound, upperBound, 2 * Math.PI / period, k);
+      const outputArr = new Float64Array(instance.HEAPF64.buffer, resPtr, 2);
+      const fail = (new Int8Array(instance.HEAP32.buffer, resPtr + 16, 1))[0]; // 16 because each double is 8 bytes in length
+      instance._free(resPtr);
+
+      const real = outputArr[0];
+      const img = outputArr[1];
+      const mag = (real ** 2 + img ** 2) ** 0.5
+      const phase = 180 * Math.atan(img / real) / Math.PI
+      if (!fail) {
+        if (k == 0) {
+          console.log(`a${k}:`, ((real) / period).toFixed(4));
+          ak.push((real) / period);
+          bk.push(false);
+        }
+        // console.log('k:',k,'=>','mag',parseFloat(mag.toFixed(5))/(2*period),'phase',parseFloat(phase.toFixed(5)));
+        else {
+          ak.push((2 * real) / period);
+          bk.push((2 * img / period));
+          console.log('k:', k, '=>', `a${k}:`, ((2 * real) / period).toFixed(4), `b${k}:`, (2 * img / period).toFixed(4));
+
+        }
+      }
+    }
+    return { ak, bk };
+  });
+}
+
+function func1(x) {
+  return x ** 3 - 12 * x + 16;
+}
+getFS(func1, 45, -6, 6).then(res => console.log(res))
+
+function func2(x) {
+  return 12 * Math.cos(2 * x) * Math.sin(x) + 16;
+}
+getFS(func2, 4, 0, 2 * Math.PI).then(res => console.log(res))
+
+
--- gsl/cppWithGSL/integral/fourierTransform/README.md
+++ gsl/cppWithGSL/integral/fourierTransform/README.md
@@ -2,18 +2,20 @@
 
 Once you have compiled gsl source code with emscripten (see /gsl/cppWithGSL/REAME.md):
 
+go into any sub folder
+
 Generate `example.js` and `example.wasm`:
 
 for Ubuntu:
 ```
 cd ../
-emcc [path/to/built/gsl]/.libs/libgsl.so.25  example.cpp -I [path/to/built/gsl] -lm -o example.js -sMODULARIZE
+emcc [path/to/built/gsl]/.libs/libgsl.so.25  example.cpp -I [path/to/built/gsl] -I ../../include/ -lm -o example.js -sMODULARIZE
 ```
 
 for MacOS:
 ```
 cd ../
-emcc [path/to/built/gsl]/.libs/libgsl.25.dylib  example.cpp -I [path/to/built/gsl] -lm -o example.js -sMODULARIZE
+emcc [path/to/built/gsl]/.libs/libgsl.25.dylib  example.cpp -I [path/to/built/gsl] -I ../../include/ -lm -o example.js -sMODULARIZE
 ```
 
 Test:
